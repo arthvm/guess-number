2b7959a5db38e788334dfe65fa53a6527ecbd3df
arthvm <90999445+arthvm@users.noreply.github.com>
2024-03-05 19:33:00 -0300

    Add visual feedback on min and max numbers

--- app/randomNumber.js
+++ app/randomNumber.js
@@ -2,8 +2,13 @@ const minNumber = 0;
 const maxNumber = 100;
 const secretNumber = generateRandomNumber();
 
+const minNumberElement = document.getElementById("min-number");
+const maxNumberElement = document.getElementById("max-number");
+
 function generateRandomNumber() {
   return parseInt(Math.random() * (maxNumber - minNumber + 1) + minNumber);
 }
 
+minNumberElement.innerHTML = minNumber;
+maxNumberElement.innerHTML = maxNumber;
 console.log("Secret Number:", secretNumber); // DEBUG PURPOSE
